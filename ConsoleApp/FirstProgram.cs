0316b83e79ef35347082fe04f2c2bcfd20404708
Ananda Murugan <m.anandsp@hotmail.com>
2017-09-07 21:25:45 +0200

    Renamed file

---
+++ ConsoleApp/FirstProgram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class FirstProgram
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome! This is my first csharp solution.");
+        }
+    }
+}
